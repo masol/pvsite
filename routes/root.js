7c50426306ec1ca191f148d4abeb5257e6a337e6
lizhutang <lizhutang@spolo.org>
2022-09-06 03:45:45 +0800

    将vault docker部署加入到pv-soa库中

--- routes/root.js
+++ routes/root.js
@@ -3,7 +3,8 @@
 module.exports = async function (fastify, opts) {
   fastify.get('/', async function (request, reply) {
     // console.log(fastify._)
-    console.log(fastify.sco)
+    const vault = await fastify.soa.get('vault')
+    fastify.log.debug('test vault from route:%o', vault)
     // console.log("fastify.static=",fastify.static)
     return { root: true }
   })
