41a7d9dd322e553541607e9b2d74341bfb03e122
masol <masol.li@gmail.com>
2022-11-21 21:46:38 +0800

    添加tengxun集群,回复test3集群

--- src/routes/example/index.js
+++ src/routes/example/index.js
@@ -3,6 +3,6 @@
 module.exports = async function (fastify, opts) {
   fastify.get('/', async function (request, reply) {
     console.log('request.session.authenticated=', request.session.authenticated)
-    return 'this is an example'
+    return 'this is an example aaaabbbbcccc'
   })
 }
