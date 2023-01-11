e2eab04b7c6fd72a2615600c9162605441e3f8e1
masol <masol.li@gmail.com>
2023-01-11 23:35:46 +0800

    更新编辑跳转路径

--- src/routes/root.js
+++ src/routes/root.js
@@ -7,7 +7,7 @@ module.exports = async function (fastify, opts) {
       const env = await soa.get('env')
       if (await env.isDev()) {
         const endPoint = `${request.protocol}:://${request.hostname}`
-        reply.redirect(302, `http://localhost:5173/ide/index.html#ep=${endPoint}`)
+        reply.redirect(302, `http://localhost:5173/ide/res/index.html#ep=${endPoint}`)
         // reply.redirect(302, `https://www.pinyan.tech/ide/index.html#ep=${endPoint}`)
         // console.log('endPoint=', endPoint)
       } else {
